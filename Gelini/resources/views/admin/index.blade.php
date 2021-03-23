454f87297a83f9786f931b1f61092cef799c4402
Cristhian Baidal <crhighlander94@gmail.com>
2021-03-23 00:18:15 -0500

    Agregando framework laravel al proyecto además, de colocar la plantilla

---
+++ Gelini/resources/views/admin/index.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+@section('title', 'Gelini')
+
+@section('content_header')
+    <h1>Tablero</h1>
+@stop
+
+@section('content')
+    <p>Panel de trabajo GELINI.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
